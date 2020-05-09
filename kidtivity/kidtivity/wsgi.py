f0a494f04cc766dbfb7ecaf0b65584dd7e8e3a78
Brandon Cantello <bcantello@gmail.com>
2020-05-08 19:57:50 -0700

    updates wsgi.py for .env file

--- kidtivity/kidtivity/wsgi.py
+++ kidtivity/kidtivity/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kidtivity.settings')
+
+project_folder = os.path.expanduser('../kidtivity-backend')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
