a25f5835ddd9c0275ab66559f56e42bad72ff72e
Brandon Cantello <bcantello@gmail.com>
2020-05-08 23:36:28 -0700

    updates git init location

--- kidtivity/apps/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from apps.authentication.views import RegistrationAPIView, LoginApiView
-
-urlpatterns = [
-    url(r'^users/register/$', RegistrationAPIView.as_view(), name='register'),
-    url(r'^users/login/$', LoginApiView.as_view(), name='login'),
-]
