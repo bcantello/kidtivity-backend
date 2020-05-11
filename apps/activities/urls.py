60918a2f2c48d9d6518a54e24149934fa10d3bf3
Brandon Cantello <bcantello@gmail.com>
2020-05-10 17:52:40 -0700

    small changes

--- apps/activities/urls.py
+++ apps/activities/urls.py
@@ -7,8 +7,8 @@ router.register('activities', ActivityViewSet, basename='activities')
 router.register('public-activities', PublicActivitiesDetail, basename='public-activities')
 
 custom_urlpatterns = [
-    url(r'public-activities/$', PublicActivities.as_view({'get': 'list'}), name='public_activities'),
-    url(r'public-activities/(?P<pk>\d+)/$', PublicActivitiesDetail.as_view({'get': 'list'}), name='public_activity_detail'),
+    url(r'public-activities/$', PublicActivities.as_view({'get': 'list'}), name='public-activities'),
+    url(r'public-activities/(?P<pk>\d+)/$', PublicActivitiesDetail.as_view({'get': 'list'}), name='public-activity-detail'),
 ]
 
 urlpatterns = router.urls
